0cc4ef70cf557a2688a020f49712227423608c9c
Ethan <es12990@ms.wasatch.edu>
2022-11-28 09:51:17 -0700

    ball
    d

---
+++ functions.js
@@ -0,0 +1,25 @@
+var radio = document.getElementById("radio").value
+
+function putToast(){
+
+    window.alert("buggon")
+
+}
+
+function stylisticChange(){
+
+    var textBox = document.getElementById("radio");
+    textBox.style.backgroundColor = "red";
+}
+
+function thanks(){
+
+    window.alert("Thank You")
+
+}
+
+function correct(){
+
+    window.alert("✓")
+
+}
